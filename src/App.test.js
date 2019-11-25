6a441d18dd4f0088bb5e262b9edbb4ed77240a6d
jimmi <mohgamalmoh@gmail.com>
2019-11-25 10:46:27 +0200

    travis test

--- src/App.test.js
+++ src/App.test.js
@@ -8,4 +8,9 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
